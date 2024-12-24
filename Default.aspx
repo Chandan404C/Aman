0a2380402d97e30be8929312848cfa4afcb2dd73
Chandan404C <chandanofficial404402@gmail.com>
2024-12-24 15:12:15 +0530

    Aman

---
+++ Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>QR Code for GPay Payment</title>
+    <script>
+        // Optional: If you want to handle click events more interactively
+        function openUPIApp() {
+            window.location.href = "upi://pay?pa=amanagarwal8700-2@okaxis&pn=AmanKumar&am=1&cu=INR&tn=PaymentForGoods";
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Scan the QR Code to Pay via Google Pay</h2>
+            <asp:Image ID="imgQRCode"  runat="server" CssClass="qrCodeImage"  />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using QRCoder;
+using System.Drawing;
+using System.IO;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            GenerateQRCode();
+        }
+    }
+
+
+    private void GenerateQRCode()
+    {
+        // Your UPI ID and other payment details
+        
+
+        // Generate QR Code
+        using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+        {
+            string upiUrl = "upi://pay?pa=amanagarwal8700-2@okaxis&pn=AmanKumar&am=1&cu=INR&tn=PaymentForGoods";
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(upiUrl, QRCodeGenerator.ECCLevel.Q);
+            using (QRCode qrCode = new QRCode(qrCodeData))
+            {
+                using (Bitmap qrCodeImage = qrCode.GetGraphic(20))
+                {
+                    // Convert QR Code to byte array
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        qrCodeImage.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                        byte[] byteImage = ms.ToArray();
+
+                        // Display QR Code in Image Control
+                        string base64Image = Convert.ToBase64String(byteImage);
+                        imgQRCode.ImageUrl = "data:image/png;base64," + base64Image;
+                        // Make the image clickable (for mobile UPI app redirection)
+                        imgQRCode.Attributes.Add("onclick", "window.location.href='" + upiUrl + "'");
+
+                    }
+                }
+            }
+        }
+    }
+
+
+}
